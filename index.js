0538365ec1f065a1724b6dc9332730fe6ee842c3
Elandig <15586732+Elandig@users.noreply.github.com>
2021-05-03 21:38:58 +0300

    Added a few useless comments to improve code readability

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const helmet = require('helmet')
 const path = require('path')
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-
-
-const { NanFibonacci } = require('./lib/nan-fibonacci/build/Release/nan_fibonacci.node')
-const PORT = process.env.PORT || 3000
-
 const app = express()
 
+const { NanFibonacci } = require('./lib/nan-fibonacci/build/Release/nan_fibonacci.node')
 
+const PORT = process.env.PORT || 3000
+
+// Middleware
 app.use(cookieParser());
 app.use(session({ secret: process.env.SECRET || "Some secret value", expires: new Date(Date.now() + (process.env.EXPIRES || 300000)) }));
 app.use(helmet({ contentSecurityPolicy: false }))
 
+// Array for NanFibonacci session instances storage
 var session_instances = []
 
 app.get('/', (req, res) => {
+  // Check if the session already exists, if not then create a new NanFibonacci instance
   if (!req.session.instance || !session_instances[req.session.instance]) {
     req.session.instance = req.sessionID
     session_instances[req.session.instance] = new NanFibonacci()
@@ -30,6 +31,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/get', (req, res) => {
+  // Check if the session already exists, if it does then call & send the result of the get() method to the user
   if (req.session.instance && session_instances[req.session.instance]) {
     res.send(session_instances[req.session.instance].get())
   } else {
@@ -38,6 +40,7 @@ app.get('/get', (req, res) => {
 })
 
 app.get('/reset', (req, res) => {
+  // Check if the session already exists, if it does then call & send the result of the reset() method to the user
   if (req.session.instance && session_instances[req.session.instance]) {
     res.send(session_instances[req.session.instance].reset())
   } else {
