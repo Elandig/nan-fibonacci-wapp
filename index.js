6012619087e4e286cbbc075110e17d972e1e255f
Elandig <15586732+Elandig@users.noreply.github.com>
2021-05-12 13:05:55 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -22,30 +22,67 @@ app.use('/static', express.static('public'))
 // Array for NanFibonacci session instances storage
 var session_instances = []
 
+const isValidArgument = (n) => { return /^[0-9]+$/.test(n) }
+
 app.get('/', (req, res) => {
-  // Check if the session already exists, if not then create a new NanFibonacci instance
-  if (!req.session.instance || !session_instances[req.session.instance]) {
+  if (!req.session.instance || !session_instances[req.session.instance][0])
     req.session.instance = req.sessionID
-    session_instances[req.session.instance] = new NanFibonacci()
-  }
+  // Creates & resets the instance on page reload
+  session_instances[req.session.instance] = [new NanFibonacci()]
+
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/get', (req, res) => {
-  // Check if the session already exists, if it does then call & send the result of the get() method to the user
-  if (req.session.instance && session_instances[req.session.instance]) {
-    res.send(session_instances[req.session.instance].get())
+// Validates the request and sends the results of get() method according to the given id
+app.get('/get/:id', (req, res) => {
+  if (req.session.instance && session_instances[req.session.instance] && isValidArgument(req.params.id)) {
+    let id = parseInt(req.params.id)
+    if (!session_instances[req.session.instance][id])
+      session_instances[req.session.instance][id] = new NanFibonacci()
+    res.send(session_instances[req.session.instance][id].get())
   } else {
-    res.send(400)
+    res.sendStatus(400)
   }
 })
 
-app.get('/reset', (req, res) => {
-  // Check if the session already exists, if it does then call & send the result of the reset() method to the user
-  if (req.session.instance && session_instances[req.session.instance]) {
-    res.send(session_instances[req.session.instance].reset())
+// Validates the request and sends the results of reset() method according to the given id
+app.get('/reset/:id', (req, res) => {
+  if (req.session.instance && session_instances[req.session.instance] && isValidArgument(req.params.id) && session_instances[req.session.instance][parseInt(req.params.id)]) {
+    res.send(session_instances[req.session.instance][parseInt(req.params.id)].reset() ? 200 : 500)
   } else {
-    res.send(400)
+    res.sendStatus(400)
+  }
+})
+
+// Validates the request and removes the instance by its id
+app.get('/delete/:id', (req, res) => {
+  if (req.session.instance &&
+    session_instances[req.session.instance] &&
+    isValidArgument(req.params.id) &&
+    parseInt(req.params.id) !== 0) {
+    if (session_instances[req.session.instance][parseInt(req.params.id)])
+      session_instances[req.session.instance][parseInt(req.params.id)] = null
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+})
+
+// Validates the request and sends the results of genFibo(amount) method
+app.get('/generate/:amount', (req, res) => {
+  if (req.session.instance && session_instances[req.session.instance] && isValidArgument(req.params.amount) && parseInt(req.params.amount) <= 50000 && parseInt(req.params.amount) > 0) {
+    res.send(session_instances[req.session.instance][0].genFibo(parseInt(req.params.amount)))
+  } else {
+    res.sendStatus(400)
+  }
+})
+
+// Validates the request and sends the results of isFibo(sequence) method
+app.get('/isfibo/:sequence', (req, res) => {
+  if (req.session.instance && session_instances[req.session.instance] && isValidArgument(req.params.sequence)) {
+    res.send(session_instances[req.session.instance][0].isFibo(req.params.sequence))
+  } else {
+    res.sendStatus(400)
   }
 })
 
--- public/script.js
+++ public/script.js
@@ -1,33 +1,219 @@
 // MIT License. Copyright (c) 2021 Elandig
+'use strict'
+const container = document.getElementById('app')
 
-const prePreviousOne = document.getElementById("counter-1")
-const previousOne = document.getElementById("counter-2")
-const counter = document.getElementById("counter-3")
-
-const updateState = (value, reset) => {
-  prePreviousOne.innerHTML = reset ? '...' : previousOne.innerHTML
-  previousOne.innerHTML = reset ? '...' : counter.innerHTML
-  counter.innerHTML = value
+const fetchData = async (url) => {
+  const req = await fetch(url)
+  if (!req.ok)
+    window.location.reload()
+  const text = await req.text()
+  return text
 }
 
-document.getElementById("getBtn").addEventListener("click", () => {
-  fetch('get').then((response) => {
-    if (!response.ok)
-      throw Error(response.statusText)
-    response.text().then(data => updateState(data.length <= 17 ? data : data + '..'))
-  })
-})
+const App = () => {
+  // Initial instances state template
+  const initState = { id: 0, enabled: true }
 
-document.getElementById("resetBtn").addEventListener("click", () => {
-  fetch('reset').then((response) => {
-    if (!response.ok)
-      throw Error(response.statusText)
-    response.text().then(data => {
-      updateState(data, true)
-      fetch('get')
+  const [instances, updateInstances] = React.useState([initState])
+
+  // Instance state reducer
+  const handleInstanceUpdate = (action, payload) => {
+    switch (action) {
+      case 'create':
+        {
+          let temp = { ...initState }
+          temp.id = instances[instances.length - 1].id + 1
+          updateInstances([...instances, temp])
+        }
+        break
+      case 'delete':
+        {
+          let temp = [...instances]
+          temp[payload.id].enabled = false
+          updateInstances(temp)
+          fetchData(`/delete/${payload.id}`)
+        }
+    }
+  }
+
+  // Initialization
+  React.useEffect(() => {
+    //for ()
+    //handleInstanceUpdate('create')
+  }, [])
+  return (
+    <>
+      {instances.map((instance) =>
+        !instance.enabled ? '' :
+          <Instance
+            key={instance.id}
+            id={instance.id}
+            onExit={() => handleInstanceUpdate('delete', { id: instance.id })} />
+      )}
+      <NewFibo onClick={() => handleInstanceUpdate('create')} />
+    </>
+  )
+}
+
+/*
+ * Main instance
+ */
+const Instance = (props) => {
+  const outputRef = React.useRef(null)
+  const [outputValue, changeOutputValue] = React.useState('Fibonacci Sequence')
+  const [genFiboValue, changeGenFiboValue] = React.useState('')
+  const [isFiboValue, changeIsFiboValue] = React.useState('')
+  const [isFiboState, changeIsFiboState] = React.useState('')
+  const [contextView, updateContextView] = React.useState(false)
+
+  React.useEffect(() => {
+    outputRef.current.scrollLeft = outputRef.current.scrollWidth
+  })
+
+  React.useEffect(() => {
+    const buttonStateTimeout = setTimeout(() => {
+      changeIsFiboState('')
+    }, 3000)
+    return () => {
+      clearTimeout(buttonStateTimeout);
+    }
+  }, [isFiboState])
+
+  const getFibo = () => {
+    fetchData(`/get/${props.id}`).then((resp) => {
+      changeOutputValue(resp)
     })
-  })
-})
+  }
 
-fetch('reset')
-fetch('get')
+  const genFibo = () => {
+    if (genFiboValue === '' || !/^[0-9]+$/.test(genFiboValue) || genFiboValue <= "0" || genFiboValue > "50000") {
+      alert('Please fill in the GenFibo field properly')
+    } else {
+      fetchData(`/generate/${genFiboValue}`).then((resp) => {
+        changeOutputValue(resp)
+      })
+    }
+  }
+  const isFibo = () => {
+    if (isFiboValue === '' || !/^[0-9]+$/.test(isFiboValue) || isFiboValue <= "0") {
+      alert('Please fill in the isFibo field properly')
+    } else {
+      fetchData(`/isfibo/${isFiboValue}`).then((resp) => {
+        if (resp === 'true') {
+          changeIsFiboState('btn-green')
+        } else {
+          changeIsFiboState('btn-red')
+        }
+      })
+    }
+  }
+
+  const resetValues = (id) => {
+    changeIsFiboValue('')
+    changeGenFiboValue('')
+    changeOutputValue('Fibonacci Sequence')
+    fetchData(`/reset/${id}`)
+  }
+
+  const genFiboInput = (event) => {
+    let field = (event.target.validity.valid && event.target.value[0] !== '0') ? event.target.value : genFiboValue
+    if (field > 50000)
+      field = '50000'
+    changeGenFiboValue(field)
+  }
+
+  const isFiboInput = (event) => {
+    let field = (event.target.validity.valid && event.target.value.length < 1000) ? event.target.value : isFiboValue
+    changeIsFiboValue(field)
+  }
+
+  return (
+    <>
+      {contextView ? <div className="overlay"></div> : ''}
+      <Panel type={`instance ${contextView ? 'context-view' : ''} ${props.id === 0 ? 'panel-first' : ''}`}>
+        {props.id === 0 || contextView ? '' : <CloseButton onClick={props.onExit} />}
+        <div ref={outputRef} className="output">{outputValue}</div>
+
+        <div className="instance-left">
+          <div>
+            <Button onClick={genFibo} text="GenFibo" />
+            <Input value={genFiboValue} onChange={genFiboInput} type="instance-left-input" text="Index" />
+          </div>
+          <Button onClick={getFibo} text="GetFibo" />
+          <div>
+            <Button type={isFiboState} onClick={isFibo} text="CheckFibo" />
+            <Input value={isFiboValue} onChange={isFiboInput} type="instance-left-input" text="Your sequence" />
+          </div>
+        </div>
+
+        <div className="instance-right">
+          <Button onClick={() => resetValues(props.id)} text="Reset" />
+          <Button onClick={() => updateContextView(!contextView)} text={contextView ? 'Close' : 'ContextView'} />
+        </div>
+
+      </Panel>
+    </>
+  )
+}
+
+/*
+ * Component for Instance creation
+ */
+const NewFibo = (props) => {
+  return (
+    <Panel type="panel-last">
+      <Button onClick={props.onClick} text="NewFibo" />
+    </Panel>
+  )
+}
+
+/*
+ * Base panel
+ */
+const Panel = (props) => {
+  const type = props.type ? props.type : ''
+  return (
+    <div className={`panel ${type}`}>
+      {props.children}
+    </div>
+  )
+}
+
+/*
+ * Base button
+ */
+const Button = (props) => {
+  const type = props.type ? props.type : ''
+  return (
+    <button className={`btn ${type}`} onClick={props.onClick} type="button">{props.text}</button>
+  )
+}
+
+/*
+ * Base input field
+ */
+const Input = (props) => {
+  const type = props.type ? props.type : ''
+  return (
+    <input
+      onInput={props.onChange}
+      pattern="[0-9]*"
+      className={`fld ${type}`}
+      type="text"
+      placeholder={props.text}
+      readOnly={props.readonly}
+      value={props.value} />
+  )
+}
+
+const CloseButton = (props) => {
+  return (
+    <div className="closeButton" onClick={props.onClick}>
+      <svg width="20" height="20" viewBox="0 0 22 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path opacity="0.48" d="M11.4531 9.93L18.2969 0.640625L21.625 2.64L14.02 13.0859L22 24L18.6953 26L11.4766 16.27L4.25781 26L0.953125 24L8.91 13.0859L1.30469 2.64L4.60938 0.640625L11.4531 9.93Z" fill="black" />
+      </svg>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, container);
